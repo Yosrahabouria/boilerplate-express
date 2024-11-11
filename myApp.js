bc33267b3dfff69d4e9dfeb879d8763dffa92c66
Yosrahabouria <yosrahabouria@gmail.com>
2024-11-11 14:23:57 +0000

    fix

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
-let express = require('express');
-let path = require('path');  // Include path module to handle file paths
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
+// Serve the index.html file on GET request to '/'
 app.get('/', (req, res) => {
   // Use __dirname to get the absolute path
   const absolutePath = path.join(__dirname, 'views', 'index.html');
   res.sendFile(absolutePath);
 });
 
-
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
